333226790186e0a24aee0c0d71b0ed5388979c1b
Sakif <sakif015sakif@gmail.com>
2024-01-11 17:52:04 +0600

    Made it a PWA

---
+++ assets/js/constants/index.js
@@ -0,0 +1,32 @@
+// Not gonna export directly
+// Not gonna export directly
+
+const theme_color = "#1e2327";
+const new_title = "Rich Kid";
+const application_name = new_title;
+const icon_path = "/assets/icons/icon.svg";
+const favicon_props = {
+	rel: "icon",
+	type: "image/svg+xml",
+	href: icon_path,
+	sizes: "any"
+};
+const sizes_array_for_apple_touch_icons = [
+	"/icons/apple-touch-icon-152x152.png",
+	"/icons/apple-touch-icon-180x180.png",
+	"/icons/apple-touch-icon-167x167.png"
+];
+const stylesheet_path = "/assets/css/main.css";
+const manifest_path = "/manifest.json";
+
+
+export {
+	theme_color,
+	new_title,
+	application_name,
+	favicon_props,
+	icon_path,
+	sizes_array_for_apple_touch_icons,
+	stylesheet_path,
+	manifest_path
+};
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,32 @@
 console.log("You're rich baby. 🍼");
+
+import {
+	theme_color,
+	new_title,
+	application_name,
+	favicon_props,
+	icon_path,
+	sizes_array_for_apple_touch_icons,
+	stylesheet_path,
+	manifest_path
+} from "./constants/index.js";
+
+import {
+	injectApplicationNameMetaTag,
+	injectManifestLinkTag,
+	injectMobileWebAppCapableMetaTag,
+	injectFavicon,
+	injectIosStatusBar,
+	injectMetaThemeTag,
+	injectTitle,
+	injectStylesheet
+} from "./lib/injections.js";
+
+injectMetaThemeTag(theme_color);
+injectStylesheet(stylesheet_path);
+injectTitle(new_title);
+injectFavicon(favicon_props);
+injectIosStatusBar(theme_color);
+injectMobileWebAppCapableMetaTag();
+injectApplicationNameMetaTag(application_name);
+injectManifestLinkTag(manifest_path);
